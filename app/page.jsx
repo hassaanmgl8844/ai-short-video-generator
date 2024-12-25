488c87b715a39f196e970bdc7ddd3fe2bfa786ad
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-12-25 19:37:33 +0500

    Basic Setup is done

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Subscribe to Luffy's Logic</h2>
+      <Button>Subscribe</Button>
+    </div>
+  );
+}
