abd7182920918baf52f8d365521954b06c4cc7fd
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-01-01 17:49:34 +0500

    40 Minutes Done

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,5 +1,15 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2">
+      <div>
+        <Image src={"/login.jpg"} alt="login" width={400} height={500} />
+      </div>
+      <div className="flex justify-center items-center h-screen">
+        <SignIn />
+      </div>
+    </div>
+  );
 }
