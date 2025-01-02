8fe503a83783a523ab7ec97d83a6db220c1680ee
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-01-02 13:34:16 +0500

    Everything Is Working Fine Upto 40 Minutes Of Video

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2">
       <div>
-        <Image src={"/login.jpg"} alt="login" width={400} height={500} />
+        <Image src={"/login.jpg"} alt="login" width={500} height={500} className="w-full object-contain" />
       </div>
       <div className="flex justify-center items-center h-screen">
         <SignIn />
