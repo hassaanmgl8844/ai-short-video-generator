a0775e764b3308df9edbf55368b20cfd15786570
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-01-03 18:20:23 +0500

    Side Nav Is Done

--- app/dashboard/_component/SideNav.jsx
+++ app/dashboard/_component/SideNav.jsx
@@ -1,18 +1,51 @@
-import { PanelsTopLeft } from 'lucide-react'
-import React from 'react'
+import { CircleUser, FileVideo, PanelsTopLeft, ShieldPlus } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 const SideNav = () => {
-    const MenuOption =[
-        {
-            id:1,
-            name:"Dashboard",
-            path:"/dashboard",
-            icon:PanelsTopLeft
-        }
-    ]
+  const MenuOption = [
+    {
+      id: 1,
+      name: "Dashboard",
+      path: "/dashboard",
+      icon: PanelsTopLeft,
+    },
+    {
+      id: 2,
+      name: "Create New",
+      path: "/create-new",
+      icon: FileVideo,
+    },
+    {
+      id: 3,
+      name: "Upgrade",
+      path: "/upgrade",
+      icon: ShieldPlus,
+    },
+    {
+      id: 4,
+      name: "Account",
+      path: "/account",
+      icon: CircleUser,
+    },
+  ];
   return (
-    <div className='w-64 h-screen shadow-md p-5'>SideNav</div>
-  )
-}
+    <div className="w-64 h-screen shadow-md p-5">
+      <div className="grid gap-3">
+        {MenuOption.map((item, index) => (
+          <Link href={item.path}>
+            <div
+              className="flex items-center gap-3 p-3
+         hover:bg-primary hover:text-white rounded-md cursor-pointer"
+            >
+              <item.icon />
+              <h2>{item.name}</h2>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default SideNav
+export default SideNav;
