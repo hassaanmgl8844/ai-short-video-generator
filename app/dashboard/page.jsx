349bb779ed83b6bfcf1a2006493387a1d3cd4e80
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-01-03 18:41:09 +0500

    1 Hour 11 Minutes Video Done

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+"use client"
+import { Button } from '@/components/ui/button'
+import React, { useState } from 'react'
+import EmptyState from './_component/EmptyState';
 
 const Dashboard = () => {
+  const [videoList,setVideoList] = useState([]);
   return (
-    <div>Dashboard</div>
+    <div>
+      <div className='flex justify-between items-center'>
+        <h2 className='font-bold text-2xl text-primary'>Dashboard</h2>
+        <Link href={"/dashboard/create-new"}>
+        <Button>+ Create New</Button>
+      </Link>
+      </div>
+
+      {/* Empty State  */}
+      {videoList?.length==0&&<div>
+          <EmptyState />
+        </div>}
+    </div>
   )
 }
 
