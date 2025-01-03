deee7823d82b14ee649d710407defb0c6a021d14
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-01-03 18:01:45 +0500

    PC Header is Ready

--- app/dashboard/_component/Header.jsx
+++ app/dashboard/_component/Header.jsx
@@ -1,14 +1,21 @@
-import Image from 'next/image'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import React from "react";
 
 const Header = () => {
   return (
-    <div>
-        <div>
-            <Image src={'/logo.svg'} width={30} height={30} />
-        </div>
-        </div>
-  )
-}
+    <div className="p-3 px-5 flex items-center justify-between shadow-md">
+      <div className="flex gap-3 items-center">
+        <Image src={"/logo.svg"} width={30} height={30} alt="logo" />
+        <h2 className="font-bold text-xl">AI Short Vid</h2>
+      </div>
+      <div className="flex gap-3 items-center">
+        <Button>Dashboard</Button>
+        <UserButton />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
