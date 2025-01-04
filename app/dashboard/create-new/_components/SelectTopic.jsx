205611c55d3a92502072ae3ca5af634f1e2393fc
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-01-04 15:08:25 +0500

    Select Working on the second part of the SideNav

---
+++ app/dashboard/create-new/_components/SelectTopic.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SelectTopic = () => {
+  return (
+    <div>SelectTopic</div>
+  )
+}
+
+export default SelectTopic
--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import SelectTopic from './_components/SelectTopic'
 
 const CreateNew = () => {
   return (
-    <div>CreateNew</div>
+    <div className='md:px-20'>
+      <h2 className='font-bold text-4xl text-primary text-center'>Create New</h2>
+
+      <div className='mt-10 shadow-md p-10'>
+        {/* Select Topic Component */}
+      <SelectTopic />
+
+        {/* Select Style Component  */}
+
+
+        {/* Duration Component  */}
+
+
+        {/* Create Button  */}
+      </div>
+    </div>
   )
 }
 
