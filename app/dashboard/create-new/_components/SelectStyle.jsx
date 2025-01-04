03360fb7f47df643d5026825e82c7b06a53d261f
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-01-04 15:58:05 +0500

    1 Hour 30 Minutes Video Done

--- app/dashboard/create-new/_components/SelectStyle.jsx
+++ app/dashboard/create-new/_components/SelectStyle.jsx
@@ -25,14 +25,21 @@ const SelectStyle = () => {
     },
   ];
   return (
-    <div>
+    <div className="mt-7">
       <h2 className="font-bold text-2xl text-primary">Style</h2>
       <p className="text-gray-500 ">Select Your Video Style?</p>
-      <div>
-        {styleOptions.map((item,index)=>(
-            <div>
-                <Image src={item.image} width={100}  height={100} alt={item.name}/>
-            </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-5 mt-3">
+        {styleOptions.map((item, index) => (
+          <div className="relative hover:scale-105 transition-all">
+            <Image
+              src={item.image}
+              width={100}
+              height={100}
+              alt={item.name}
+              className="h-48 object-cover rounded-lg w-full"
+            />
+            <h2 className="absolute p-1 bg-black text-white text-center rounded-b-lg bottom-0 w-full">{item.name}</h2>
+          </div>
         ))}
       </div>
     </div>
