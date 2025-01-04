d1e3b376387ec2020b49ce791666c3fb46bbc909
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-01-04 15:17:04 +0500

    1 Hour 17 Minutes Video DOne

--- app/dashboard/create-new/_components/SelectTopic.jsx
+++ app/dashboard/create-new/_components/SelectTopic.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const SelectTopic = () => {
+    const options=['Custom Prompt','Random AI Story','Scary Story','Historical Facts','Bed Time Story','Motivational','Fun Facts']
   return (
-    <div>SelectTopic</div>
-  )
-}
+    <div>
+      <h2 className="font-bold text-2xl text-primary">Content</h2>
+      <p className="text-gray-500 ">What is the topic of your video?</p>
 
-export default SelectTopic
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Content Type" />
+        </SelectTrigger>
+        <SelectContent>
+            {options.map((item,index)=>(
+                <SelectItem key={index} value={item}>{item}</SelectItem>
+            ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default SelectTopic;
