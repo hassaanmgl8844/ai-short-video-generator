e494c4ab1e89b1c9f83e034976c28bb4fcf35a61
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-01-04 15:50:03 +0500

    1 Hour 27 Minutes Done

--- app/dashboard/create-new/_components/SelectStyle.jsx
+++ app/dashboard/create-new/_components/SelectStyle.jsx
@@ -1,31 +1,42 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const SelectStyle = () => {
-    const styleOptions = [
-        {
-            name:'Realstic',
-            image: 'real.jpeg',
-        },
-        {
-            name:'Cartoon',
-            image: 'cartoon.jpg',
-        },
-        {
-            name:'Comic',
-            image: 'comic.webp',
-        },
-        {
-            name:'WaterColor',
-            image: 'WaterColor.jpg',
-        },
-        {
-            name:'GTA',
-            image: 'gta.jpg',
-        },
-    ]
+  const styleOptions = [
+    {
+      name: "Realstic",
+      image: "/real.jpeg",
+    },
+    {
+      name: "Cartoon",
+      image: "/cartoon.jpg",
+    },
+    {
+      name: "Comic",
+      image: "/comic.webp",
+    },
+    {
+      name: "WaterColor",
+      image: "/WaterColor.jpg",
+    },
+    {
+      name: "GTA",
+      image: "/gta.jpg",
+    },
+  ];
   return (
-    <div>SelectStyle</div>
-  )
-}
+    <div>
+      <h2 className="font-bold text-2xl text-primary">Style</h2>
+      <p className="text-gray-500 ">Select Your Video Style?</p>
+      <div>
+        {styleOptions.map((item,index)=>(
+            <div>
+                <Image src={item.image} width={100}  height={100} alt={item.name}/>
+            </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default SelectStyle
+export default SelectStyle;
