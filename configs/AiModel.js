f4ee23ef974c5f17e9c6c872e275f7134500d727
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-01-05 22:24:18 +0500

    1 Hour 46 Minute

--- configs/AiModel.js
+++ configs/AiModel.js
@@ -4,7 +4,7 @@ const {
     HarmBlockThreshold,
   } = require("@google/generative-ai");
   
-  const apiKey = process.env.GEMINI_API_KEY;
+  const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
@@ -16,11 +16,10 @@ const {
     topP: 0.95,
     topK: 40,
     maxOutputTokens: 8192,
-    responseMimeType: "text/plain",
+    responseMimeType: "application/json",
   };
   
-  async function run() {
-    const chatSession = model.startChat({
+   export const chatSession = model.startChat({
       generationConfig,
       history: [
         {
@@ -38,8 +37,3 @@ const {
       ],
     });
   
-    const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
-    console.log(result.response.text());
-  }
-  
-  run();
