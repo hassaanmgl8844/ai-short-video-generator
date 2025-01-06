6dc42fd3089ca5875e18bdf10092f53732558bbe
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-01-06 09:02:54 +0500

    Generate Video Script with the help of Gemini DOne

--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -23,17 +23,25 @@ const CreateNew = () => {
   };
 
   // Get Video Script
-  const  GetVideoScript=async()=>{
-    const prompt='Wriate a script to generate '+formData.duration+' video on topic : '+formData.topic+'  along with AI image prompt in '+formData.imageStyle+' format  for each scene and give me result in JSON format with imagePrompt and ContentText as field, No Plain Text';
+  const GetVideoScript = async () => {
+    const prompt =
+      "Wriate a script to generate " +
+      formData.duration +
+      " video on topic : " +
+      formData.topic +
+      "  along with AI image prompt in " +
+      formData.imageStyle +
+      " format  for each scene and give me result in JSON format with imagePrompt and ContentText as field, No Plain Text";
     console.log(prompt);
-    
-    const result = await axios.post('/api/get-video-script', {
-      prompt: prompt
-    }).then(resp=>{
-      console.log(resp.data);
-      
-    })
-      }
+
+    const result = await axios
+      .post("/api/get-video-script", {
+        prompt: prompt,
+      })
+      .then((resp) => {
+        console.log(resp.data);
+      });
+  };
 
   return (
     <div className="md:px-20">
@@ -49,7 +57,9 @@ const CreateNew = () => {
         {/* Duration Component  */}
         <SelectDuration onUserSelect={onHandleInputChange} />
         {/* Create Button  */}
-        <Button className="mt-10 w-full" onClick={onCreateClickHandler}>Create Short Video</Button>
+        <Button className="mt-10 w-full" onClick={onCreateClickHandler}>
+          Create Short Video
+        </Button>
       </div>
     </div>
   );
